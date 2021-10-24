5ddb25dec0f4ac891fd2a9979af714a0495bd165
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2021-10-24 16:54:00 +0800

    add HelloWorld.cpp

---
+++ Eclipse/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+/**
+ * @author wargrey
+ *
+ */
+module eclipse {
+}
---
+++ Eclipse/src/stem/wargrey/HelloWorld.java
@@ -0,0 +1,12 @@
+
+package stem.wargrey;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// 在终端输出字符串 "Hello, World！"
+
+		System.out.println("Hello, World!");
+	}
+
+}
