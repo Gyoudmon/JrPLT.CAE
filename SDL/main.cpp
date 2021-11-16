8b90a66d3f414fdd4a617d21ee4c42640d71fcd0
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2021-11-16 13:15:14 +0800

    create SDL window from an empty project

--- SDL/main.cpp
+++ SDL/main.cpp
@@ -1,113 +1,36 @@
-#include <exception>
-#include <string>
 #include <iostream>
 #include <SDL.h>
 
-class InitError : public std::exception
-{
-    std::string msg;
-public:
-    InitError();
-    InitError( const std::string & );
-    virtual ~InitError() throw();
-    virtual const char * what() const throw();
-};
+/*************************************************************************************************/
+// 窗口尺寸常量
+const int SCREEN_WIDTH  = 640;
+const int SCREEN_HEIGHT = 480;
 
-InitError::InitError() :
-    exception(),
-    msg( SDL_GetError() )
-{
-}
+int SDL_main( int argc, char* args[] ){
+    SDL_Window* window = NULL;          // SDL 窗口
+    SDL_Surface* screenSurface = NULL;  // SDL 窗口里的屏幕
 
-InitError::InitError( const std::string & m ) :
-    exception(),
-    msg( m )
-{
-}
+    // 初始化 SDL
+    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+        std::cout << "SDL_Error: " << SDL_GetError() << std::endl;
+    } else {
+        // 创建 SDL 窗口
+        window = SDL_CreateWindow( "Hello, RGB", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                   SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 
-InitError::~InitError() throw()
-{
-}
+        if ( window == NULL ) {
+            std::cout << "SDL_Error: " << SDL_GetError() << std::endl;
+        } else {
+            screenSurface = SDL_GetWindowSurface( window ); // 将“窗口屏幕”赋值给变量 screenSurface
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0xFF, 0xFF ) ); // 给屏幕填色
 
-const char * InitError::what() const throw()
-{
-    return msg.c_str();
-}
-
-class SDL
-{
-    SDL_Window * m_window;
-    SDL_Renderer * m_renderer;
-public:
-    SDL( Uint32 flags = 0 );
-    virtual ~SDL();
-    void draw();
-};
-
-SDL::SDL( Uint32 flags )
-{
-    if ( SDL_Init( flags ) != 0 )
-        throw InitError();
-
-    if ( SDL_CreateWindowAndRenderer( 640, 480, SDL_WINDOW_SHOWN,
-                                      &m_window, &m_renderer ) != 0 )
-        throw InitError();
-}
-
-SDL::~SDL()
-{
-    SDL_DestroyWindow( m_window );
-    SDL_DestroyRenderer( m_renderer );
-    SDL_Quit();
-}
-
-void SDL::draw()
-{
-    // Clear the window with a black background
-    SDL_SetRenderDrawColor( m_renderer, 0, 0, 0, 255 );
-    SDL_RenderClear( m_renderer );
-
-    // Show the window
-    SDL_RenderPresent( m_renderer );
-
-    int rgb[] = { 203, 203, 203, // Gray
-                  254, 254,  31, // Yellow
-                    0, 255, 255, // Cyan
-                    0, 254,  30, // Green
-                  255,  16, 253, // Magenta
-                  253,   3,   2, // Red
-                   18,  14, 252, // Blue
-                    0,   0,   0  // Black
-                };
-
-    SDL_Rect colorBar;
-    colorBar.x = 0; colorBar.y = 0; colorBar.w = 90; colorBar.h = 480;
-
-    // Render a new color bar every 0.5 seconds
-    for ( int i = 0; i != sizeof rgb / sizeof *rgb; i += 3, colorBar.x += 90 )
-    {
-        SDL_SetRenderDrawColor( m_renderer, rgb[i], rgb[i + 1], rgb[i + 2], 255 );
-        SDL_RenderFillRect( m_renderer, &colorBar );
-        SDL_RenderPresent( m_renderer );
-        SDL_Delay( 500 );
-    }
-}
-
-int main( int argc, char * argv[] )
-{
-    try
-    {
-        SDL sdl( SDL_INIT_VIDEO | SDL_INIT_TIMER );
-        sdl.draw();
-
-        return 0;
-    }
-    catch ( const InitError & err )
-    {
-        std::cerr << "Error while initializing SDL:  "
-                  << err.what()
-                  << std::endl;
+            SDL_UpdateWindowSurface( window );  // 更新窗体
+            SDL_Delay( 2000 );                  // 等待两秒种
+        }
     }
 
-    return 1;
+    SDL_DestroyWindow( window );    // 销毁 SDL 窗口
+    SDL_Quit();                     // 退出 SDL 系统
+
+    return 0;
 }
