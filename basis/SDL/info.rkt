e60f66bd5e3293146a27a8d1073b73f8b7418454
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-01-02 16:17:41 +0800

    no need to configure for MSVC in details

--- basis/SDL/info.rkt
+++ basis/SDL/info.rkt
@@ -7,10 +7,7 @@
 (define pkg-authors '(wargrey))
 (define test-omit-paths 'all)
 
-(define msvc-kits-rootdir "C:\\Program Files (x86)\\Windows Kits\\10")
-(define msvc-kits-version "10.0.19041.0")
-
-(define native-launcher-names '(["main.cpp"
+(define native-launcher-names '(["main.cpp" desktop
                                  ((config SDL2)
                                   (include (windows "C:\\opt\\SDL2-2.0.18-vc\\include"))
                                   (libpath (windows "C:\\opt\\SDL2-2.0.18-vc\\lib\\x64")))]))
