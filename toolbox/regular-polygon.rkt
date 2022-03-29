f3df975fb9a252a9516c7271bbc81d29b3676fd7
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-03-30 01:55:25 +0800

    add regular-polygon

---
+++ toolbox/regular-polygon.rkt
@@ -0,0 +1,19 @@
+#lang typed/racket
+
+(require bitmap)
+
+(define R : Flonum 64.0)
+
+(define regular-polygon : (-> Index Flonum Bitmap)
+  (lambda [n pi-m]
+    (bitmap-cc-superimpose (bitmap-regular-polygon n R (* pi pi-m) #:inscribed? #true #:fill 'darkorange)
+                           (bitmap-circle R #:fill 'ghostwhite))))
+
+(define polygon-r3 (regular-polygon 3 -0.5))
+(define polygon-r4 (regular-polygon 4 -0.25))
+(define polygon-r6 (regular-polygon 6 -0.5))
+
+(module+ main
+  polygon-r3
+  polygon-r4
+  polygon-r6)
