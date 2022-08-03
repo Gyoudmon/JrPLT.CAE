ca1edf171da9997e5b1f9e9b684b6c4777e06b8b
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-08-03 11:35:19 +0800

    turns out to be ugly because of vcpkg

--- C++/basis/SDL/game.hpp
+++ C++/basis/SDL/game.hpp
@@ -8,6 +8,20 @@
 #include <cstdint>
 #include <string>
 
+/**
+ * 为兼容 Windows 而变得丑陋
+ *
+ * vcpkg 不直接提供 SDL2main
+ * 而 SDL2.h 又把 main 改名为 SDL_main 了
+ * 导致 MSVC 连接器找不到程序入口
+ *
+ * 手动链接会引发别的问题，比如跟默认库冲突
+ * (否则 vcpkg 作者肯定脑子有坑才会整出“手动链接”这种脱了裤子的那啥的活)
+ */
+#ifdef main
+#undef main
+#endif
+
 namespace WarGrey::STEM {
     /**********************************************************************************************/
     class Universe {
