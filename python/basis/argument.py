1963e7a418b53b4421c5f0f0593e54dbdc0b2045
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-09-06 23:33:59 +0800

    add argc to python.main

--- python/basis/argument.py
+++ python/basis/argument.py
@@ -3,17 +3,18 @@
 import sys # 系统相关参数和函数
 import os  # 操作系统相关函数
 
-def main(argv):
+def main(argc, argv):
     ''' 程序入口函数，不一定非得命名为 main，但习惯上应该如此
+    :param argc: "argument count" 的缩写, 即"参数个数"的意思
     :param argv: "argument vector" 的缩写, 即"参数数组"的意思，包含命令行的所有输入参数
     '''
 
     # 第一个参数一定是程序的名字(也就是这个模块的名字)
     print(argv[0])
-    print("  received {} arguments from user".format(len(argv) - 1))
+    print("  received {} arguments from user".format(argc - 1))
 
     # 现实所有用户输入参数，一行一个
-    for i in range(1, len(argv)):
+    for i in range(1, argc):
         print("    argv[{}]: {}".format(i, argv[i]))
 
     return 0
@@ -21,7 +22,8 @@ def main(argv):
 
 # __name__ 是一个特殊变量
 # 可用于提示是否从这行代码启动应用程序
-# 效果上相当于 C++ 的 main 函数
+# 效果上相当于 C++ 程序的启动器，负责调用 main 函数
 if __name__=="__main__":
-    os._exit(main(sys.argv))
+    os._exit(main(len(sys.argv), sys.argv))
+
 
--- python/basis/main.py
+++ python/basis/main.py
@@ -3,7 +3,7 @@
 import sys # 系统相关参数和函数
 import os  # 操作系统相关函数
 
-def main(argv):
+def main(argc, argv):
     print("Hello, World!")
     return 0
 
@@ -11,7 +11,8 @@ def main(argv):
 
 # __name__ 是一个特殊变量
 # 可用于提示是否从这行代码启动应用程序
-# 效果上相当于 C++ 的 main 函数
+# 效果上相当于 C++ 程序的启动器，负责调用 main 函数
 if __name__=="__main__":
-    os._exit(main(sys.argv))
+    os._exit(main(len(sys.argv), sys.argv))
+
 
