b00ef1b5c8d8fb09d9184cc2aedd2e7cbafdef66
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-10-26 19:02:45 +0800

    add char_statistics.py

---
+++ python/STEMA/char_statistics.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+###############################################################
+def make_stat_dict():
+    return { '英文字母': 0, '数字': 0, '空格': 0, '其他字符': 0 }
+
+def categorize_char(ds, c):
+    if c.isalpha():
+        ds['英文字母'] += 1
+    elif c.isspace():
+        ds['空格'] += 1
+    elif c.isdigit():
+        ds['数字'] += 1
+    else:
+        ds['其他字符'] += 1
+
+def display_stats(ds):
+    print(ds.values())
+    print(ds['英文字母'])
+    print(ds['空格'])
+    print(ds['数字'])
+    print(ds['其他字符'])
+
+###############################################################
+def main():
+    src = input()
+    ds = make_stat_dict()
+    for c in src:
+        categorize_char(ds, c)
+    display_stats(ds)
+    
+
+
+
+###############################################################
+if __name__ == "__main__":
+    main()
+
