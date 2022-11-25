43dc56db0693510c238590b67a69d06514d6a5d4
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-11-25 19:39:11 +0800

    add GenEnumClass.py

---
+++ python/basis/GenEnumClass.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from digitama.datum.uuid import *
+
+import sys
+
+def main(argv):
+    argc = len(argv)
+
+    if argc > 2:
+        ids = []
+        for i in range(2, len(argv)):
+            id = pk64_random()
+            while id in ids:
+                print("Retrying due to a hash conflict: %s" % id)
+            ids.append(id)
+
+        print("class %s(object):" % argv[1])
+        for i in range(0, len(ids)):
+            print("    %s = 0x%x" % (argv[i + 2], ids[i]))
+
+if __name__=="__main__":
+    sys.exit(main(sys.argv))
+
+
