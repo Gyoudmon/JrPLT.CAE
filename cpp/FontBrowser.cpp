42175b06d0e88e5a7667ddbe2541550e254ac281
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-03-05 15:38:27 +0800

    work with `is_font_okay`

--- cpp/FontBrowser.cpp
+++ cpp/FontBrowser.cpp
@@ -5,52 +5,72 @@ using namespace WarGrey::STEM;
 
 /*************************************************************************************************/
 namespace {
+    enum class CmdOpt { FontSize, ColumnCount, _ };
+
     class FontBrowser : public Universe {
         public:
             FontBrowser() : Universe("字体浏览器", 0) {}
         
         public:
-            void construct(int argc, char* argv[]) {
-                this->fontsize = 0;
-                this->col_count = 0;
+            void construct(int argc, char* argv[]) override {
+                for (int idx = 1; idx < argc; idx ++) {
+                    if (this->option == CmdOpt::FontSize) {
+                        this->fontsize = atoi(argv[idx]);
+                        this->option = CmdOpt::_;
+                    } else if (this->option == CmdOpt::ColumnCount) {
+                        this->col_count = atoi(argv[idx]);
+                        this->option = CmdOpt::_;
+                    } else if (strncmp(argv[idx], "-s", 3) == 0) {
+                        this->option = CmdOpt::FontSize;
+                    } else if (strncmp(argv[idx], "-c", 3) == 0) {
+                        this->option = CmdOpt::ColumnCount;
+                    } else {
+                        if (!this->text.empty()) {
+                            this->text.push_back(' ');
+                        }
 
-                if (argc > 1) {
-                    this->fontsize = atoi(argv[1]);
-
-                    if (argc > 2) {
-                        this->col_count = atoi(argv[2]);
+                        this->text.append(argv[idx]);
                     }
                 }
 
                 if (this->fontsize <= 0) {
-                    this->fontsize = 12;
+                    this->fontsize = 16;
                 }
 
                 if (this->col_count <= 0) {
-                    this->col_count = 5;
+                    this->col_count = 4;
+                }
+
+                if (this->text.empty()) {
+                    this->text = "Sphinx [字体陈列馆]";
                 }
             }
 
-            void draw(SDL_Renderer* renderer, int x, int y, int width, int height) {
+            void draw(SDL_Renderer* renderer, int x, int y, int width, int height) override {
                 int font_count;
                 const std::string* fonts = game_font_list(&font_count);
 
                 for (int i = 0; i < font_count; i++) {
                     TTF_Font* f = game_create_font(fonts[i].c_str(), this->fontsize);
-                    std::string text = game_create_string("%s (字体陈列馆)", fonts[i].c_str());
 
-                    game_draw_blended_text(f, renderer, this->get_foreground_color(), x, y, text.c_str());
-                    game_font_destroy(f);
-
-                    y = y + this->fontsize + 2;
-                    if (y > height - this->fontsize) {
-                        x = x + width / this->col_count;
-                        y = 0;
+                    if (is_font_okay(f, this->text)) {
+                        game_draw_blended_text(f, renderer, this->get_foreground_color(),
+                            x, y, "%s: %s", fonts[i].c_str(), text.c_str());
+                        
+                        y = y + this->fontsize + 2;
+                        if (y > height - this->fontsize) {
+                            x = x + width / this->col_count;
+                            y = 0;
+                        }
                     }
+
+                    game_font_destroy(f);
                 }
             }
 
         private:
+            CmdOpt option = CmdOpt::_;
+            std::string text;
             int fontsize;
             int col_count;
     };
