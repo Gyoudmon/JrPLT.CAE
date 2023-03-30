9e466f2479f3efe98942f685a677903d1351db14
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-03-30 18:05:59 +0800

    finish the first 4 Bracers

---
+++ racket/village/utility/trail.sprite/action.cp.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define trail-sprite-action-copy
+  (lambda [act-name srcdir destdir]
+    (for ([seq (in-naturals 1)]
+          [img (in-list (directory-list srcdir #:build? #false))])
+      (define .ext (path-get-extension img))
+      (define name (format "~a_~a~a" act-name (~r seq #:min-width 3 #:pad-string "0") .ext))
+      (printf "cp ~a ~a~n" img name)
+      (rename-file-or-directory (build-path srcdir img)
+                                (build-path destdir name)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(module+ main
+  (define argv (current-command-line-arguments))
+
+  (when (= (vector-length argv) 3)
+    (trail-sprite-action-copy (vector-ref argv 0)
+                              (vector-ref argv 1)
+                              (vector-ref argv 2))))
--- racket/village/utility/trail.sprite/motion.cp.rkt
+++ racket/village/utility/trail.sprite/motion.cp.rkt
@@ -3,10 +3,10 @@
 (define directions '#(w wn n en e es s ws))
 
 (define trail-sprite-moving-rename
-  (lambda [act-name rootdir]
+  (lambda [act-name srcdir destdir]
     (define dir-count (vector-length directions))
     (define action-groups
-      (let split ([ls (directory-list rootdir #:build? #false)]
+      (let split ([ls (directory-list srcdir #:build? #false)]
                   [stca null])
         (cond [(null? ls) (reverse stca)]
               [(<= (length ls) dir-count) (split null (cons ls stca))]
@@ -18,14 +18,15 @@
             [dir (in-vector directions)])
         (define .ext (path-get-extension act))
         (define name (format "~a_~a_~a~a" act-name dir (~r seq-idx #:min-width 3 #:pad-string "0") .ext))
-        (printf "mv ~a ~a~n" act name)
-        (rename-file-or-directory (build-path rootdir act)
-                                  (build-path rootdir name))))))
+        (printf "cp ~a ~a~n" act name)
+        (rename-file-or-directory (build-path srcdir act)
+                                  (build-path destdir name))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (module+ main
   (define argv (current-command-line-arguments))
 
-  (when (> (vector-length argv) 0)
-    (for ([rootdir (in-vector argv 1)])
-      (trail-sprite-moving-rename (vector-ref argv 0) rootdir))))
+  (when (= (vector-length argv) 3)
+    (trail-sprite-moving-rename (vector-ref argv 0)
+                                (vector-ref argv 1)
+                                (vector-ref argv 2))))
