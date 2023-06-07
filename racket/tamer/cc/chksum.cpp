58f4a1dcac2eb40c2a8ec510d4cbfaece18ec724
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-06-08 01:08:12 +0800

    finish testing IPv4 checksum

--- racket/tamer/cc/chksum.cpp
+++ racket/tamer/cc/chksum.cpp
@@ -1,7 +1,24 @@
-﻿#include "../../digitama/big_bang/wormhole/checksum/crc32.hpp"
+#include "../../digitama/big_bang/wormhole/checksum/crc32.hpp"
+#include "../../digitama/big_bang/wormhole/checksum/ipv4.hpp"
+
 #include "../../digitama/big_bang/datum/string.hpp"
 
 using namespace WarGrey::STEM;
+
+/*************************************************************************************************/
+static void checksum_ipv4_set(uint8_t* header, size_t size, size_t chksum_idx) {
+	size_t post_chksum_idx = chksum_idx + 2;
+	uint8_t* fields_before_checksum = header;
+	uint8_t* fields_after_checksum = &header[post_chksum_idx];
+	uint16_t sum = 0U;
+
+	checksum_ipv4(&sum, fields_before_checksum, 0, chksum_idx);
+	checksum_ipv4(&sum, fields_after_checksum, 0, size - post_chksum_idx);
+		
+	// set the checksum
+	header[chksum_idx + 0] = sum >> 8U;
+	header[chksum_idx + 1] = sum & 0xFFU;
+}
 
 /*************************************************************************************************/
 extern "C" {
@@ -24,49 +41,58 @@ extern "C" {
 		
 		return checksum_crc32(acc_crc, reinterpret_cast<const uint8_t*>(part3), 0, psz3);
 	}
+	
+	__ffi__ uint16_t hex_chksum_ipv4(const char* message, size_t size) {
+		return checksum_ipv4(reinterpret_cast<const uint8_t*>(message), 0, size);
+	}
+
+	__ffi__ uint16_t acc_chksum_ipv4(const char* message, size_t size, size_t bidx) {
+		uint16_t acc_sum = 0U;
+
+		if ((bidx & 0x01) == 1) { // odd
+			checksum_ipv4(&acc_sum, reinterpret_cast<const uint8_t*>(message), bidx, size);
+			acc_sum = ((acc_sum & 0xFF) << 8) ^ (acc_sum >> 8); // swap 2nd part checksum
+			checksum_ipv4(&acc_sum, reinterpret_cast<const uint8_t*>(message), 0, bidx);
+		} else {
+			checksum_ipv4(&acc_sum, reinterpret_cast<const uint8_t*>(message), 0, bidx);
+			checksum_ipv4(&acc_sum, reinterpret_cast<const uint8_t*>(message), bidx, size);
+		}
+
+		return acc_sum;
+	}
+
+	__ffi__ uint16_t chksum_ipv4_verify(char* header, size_t size) {
+		static const size_t checksum_idx = 10;
+
+		if (size > checksum_idx + 2) {
+			checksum_ipv4_set(reinterpret_cast<uint8_t*>(header), size, checksum_idx);
+		}
+
+		return checksum_ipv4(reinterpret_cast<uint8_t*>(header), 0, size);
+	}
+
+	__ffi__ uint16_t chksum_ipv4_true_form(char* header, size_t size) {
+		static const size_t checksum_idx = 10;
+		uint8_t* uheader = reinterpret_cast<uint8_t*>(header);
+		uint32_t Hsum = 0U;
+		uint32_t Lsum = 0U;
+		uint32_t sum = 0U;
+
+		if (size > checksum_idx + 2) {
+			checksum_ipv4_set(uheader, size, checksum_idx);
+		}
+
+		if ((size & 0x01) == 0x01) {
+			Hsum += uheader[--size];
+		}
+
+		for (size_t idx = 0; idx < size; idx += 2) {
+			Hsum += uheader[idx];
+			Lsum += uheader[idx + 1];
+		}
+
+		sum = (Hsum << 8U) ^ Lsum;
+
+		return (sum >> 16U) ^ (sum & 0xFFFFU);
+	}
 }
-
-/*
-	private class IPv4 : public TestClass<IPv4> {
-	public:
-		TEST_METHOD(Break) {
-			uint8 vector[] = { 0x00, 0x01, 0xf2, 0x03, 0xf4, 0xf5, 0xf6, 0xf7 };
-			size_t count = sizeof(vector) / sizeof(uint8);
-			unsigned short checksum = checksum_ipv4(vector, 0, count);
-
-			for (size_t bidx = 0; bidx < count; bidx++) {
-				Platform::String^ message = make_wstring(L"break@%d", bidx);
-				unsigned short acc_sum = 0;
-
-				if ((bidx & 0x01) == 1) {
-					checksum_ipv4(&acc_sum, vector, bidx, count);
-					acc_sum = ((acc_sum & 0xFF) << 8) ^ (acc_sum >> 8); // swap 2nd part checksum
-					checksum_ipv4(&acc_sum, vector, 0, bidx);
-				} else {
-					checksum_ipv4(&acc_sum, vector, 0, bidx);
-					checksum_ipv4(&acc_sum, vector, bidx, count);
-				}
-
-				Assert::AreEqual(int(checksum), int(acc_sum), message->Data());
-			}
-		}
-
-		TEST_METHOD(Head) {
-			uint8 header[] = {
-				0x45, 0x00, 0x00, 0x73, 0x00, 0x00, 0x40, 0x00, 0x40, 0x11,
-				0x00, 0x00, 0xc0, 0xa8, 0x00, 0x01, 0xc0, 0xa8, 0x00, 0xc7,
-				0x00 // termination
-			};
-			size_t checksum_idx = sizeof(header) / sizeof(uint8) / 2;
-			unsigned short sum = 0U;
-
-			checksum_ipv4(&sum, header, 0, checksum_idx);
-			checksum_ipv4(&sum, header, checksum_idx + 2, checksum_idx * 2);
-			
-			header[checksum_idx + 0] = sum >> 8U;
-			header[checksum_idx + 1] = sum & 0xFFU;
-
-			Assert::AreEqual(0x0, int(checksum_ipv4(header)), L"verify the header's checksum");
-		}
-	};
-*/
--- racket/tamer/spec.rkt
+++ racket/tamer/spec.rkt
@@ -1,24 +1,44 @@
 #lang typed/racket/base
 
 (require digimon/spec)
-(require digimon/checksum)
 
 (require "cc/chksum.rkt")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define ipv4-headers
+  (list (bytes #x45 #x00 #x00 #x73 #x00 #x00 #x40 #x00 #x40 #x11
+               #x00 #x00 #xc0 #xa8 #x00 #x01 #xc0 #xa8 #x00 #xc7)
+        (bytes #x45 #x00 #x00 #x1c #x74 #x68 #x00 #x00 #x80 #x11
+               #x59 #x8f #xc0 #xa8 #x64 #x01 #xab #x46 #x9c #xe9)))
+
+(define message
+  (bytes #x00 #x01 #xf2 #x03 #xf4 #xf5 #xf6 #xf7))
+
 (define-feature prelude #:do
-  (describe "checksum" #:do
-            (describe "CRC32" #:do
-              (it "should return zero for empty message" #:do
-                (expect-bytes= #"00000000" (str_chksum_crc32 #"")))
-              (it "should return '0x414FA339' for 'The quick brown fox jumps over the lazy dog'" #:do
-                (expect-bytes= #"414FA339" (str_chksum_crc32 #"The quick brown fox jumps over the lazy dog")))
-              (it "should return '0x7E450C04' for '73871727080876A0'" #:do
-                (expect-0x= #x7E450C04 (hex_chksum_crc32 #"73871727080876A0")))
-              (it "should return '0x2e8885d5' for 'NO4D061320000830BEB9BFE3C7C6CE68B16411FD09F96982'" #:do
-                (expect-0x= #x2e8885d5 (hex_chksum_crc32 #"NO4D061320000830BEB9BFE3C7C6CE68B16411FD09F96982")))
-              (it "should return '0x2e8885d5' for the splitted long message" #:do
-                (expect-0x= #x2e8885d5 (acc_chksum_crc32 #"NO4D0613" #"20000830" #"BEB9BFE3C7C6CE68B16411FD09F96982"))))))
+  (describe "Checksum" #:do
+    (describe "CRC32" #:do
+      (it "should return zero for empty message" #:do
+        (expect-bytes= (str_chksum_crc32 #"") #"00000000"))
+      (it "should return '0x414FA339' for 'The quick brown fox jumps over the lazy dog'" #:do
+        (expect-bytes= (str_chksum_crc32 #"The quick brown fox jumps over the lazy dog") #"414FA339"))
+      (it "should return '0x7E450C04' for '73871727080876A0'" #:do
+        (expect-0x= (hex_chksum_crc32 #"73871727080876A0") #x7E450C04))
+      (it "should return '0x2e8885d5' for 'NO4D061320000830BEB9BFE3C7C6CE68B16411FD09F96982'" #:do
+        (expect-0x= (hex_chksum_crc32 #"NO4D061320000830BEB9BFE3C7C6CE68B16411FD09F96982") #x2e8885d5))
+      (it "should return '0x2e8885d5' for the splitted long message" #:do
+        (expect-0x= (acc_chksum_crc32 #"NO4D0613" #"20000830" #"BEB9BFE3C7C6CE68B16411FD09F96982") #x2e8885d5)))
+    (describe "ipv4/tcp/udp" #:do
+      (it "should be accumulative" #:do
+        (let ([sum (hex_chksum_ipv4 message)])
+          (for ([idx (in-range (bytes-length message))])
+            (expect-= (acc_chksum_ipv4 message idx) sum
+                      (format "broken@~a" idx)))))
+      (it "should return zero for header's 1's complement code when verifying the checksum" #:do
+        (for ([header (in-list ipv4-headers)])
+          (expect-zero (chksum_ipv4_verify header))))
+      (it "should return '0xFFFF' for header's tru-form sum when verifying the checksum" #:do
+        (for ([header (in-list ipv4-headers)])
+          (expect-= (chksum_ipv4_true_form header) #xFFFF))))))
 
 
 
