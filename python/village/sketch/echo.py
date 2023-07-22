ed0801024863903433ee57040fb8ac51ce81d763
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-07-22 10:59:47 +0800

    remove the `main`, use `echo` instead

--- python/village/sketch/echo.py
+++ python/village/sketch/echo.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python3
-
 import sys # 系统相关参数和函数
 
 def main(argc, argv):
     """ 程序入口函数，不一定非得命名为 main, 此处仅为与习惯对比
 
     :param argc: 'argument count' 的缩写, 即'参数个数'的意思
-    :param argv: 'argument vector' 的缩写, 即'参数数组'的意思，包含命令行的所有输入参数
+    :param argv: 'argument vector' 的缩写, 即'参数数组'的意思
     """
 
-    # 第一个参数一定是程序的名字(也就是这个源码文件的名字)
-    print("the running program is " + argv[0])
+    # 第一个参数一定是正在运行的程序的路径
+    print("Running: " + argv[0])
     print("  received %s arguments from user" % (argc - 1))
 
     # 显示所有用户输入参数，一行一个
@@ -19,12 +17,8 @@ def main(argc, argv):
 
     return 0
 
-
-
 # __name__ 是一个特殊变量
 # 可用于提示是否从这行代码启动应用程序
 # 效果上相当于 C++ 程序的启动器，负责调用 main 函数
 if __name__ == "__main__":
     sys.exit(main(len(sys.argv), sys.argv))
-
-
